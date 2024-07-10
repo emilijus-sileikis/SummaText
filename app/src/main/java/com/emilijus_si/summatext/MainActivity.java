b56cb5ccf7258e51ee1c3b04fb8458f19571517b
Emilijus <emilijus.sileikis@mif.stud.vu.lt>
2024-07-10 17:11:07 +0300

    Gradle rework

---
+++ app/src/main/java/com/emilijus_si/summatext/MainActivity.java
@@ -0,0 +1,45 @@
+package com.emilijus_si.summatext;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        EditText inputField = findViewById(R.id.inputField);
+
+        Button submitButton = findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userInput = inputField.getText().toString().trim();
+                String summary = summarizeText(userInput);
+                Log.d("RESULT: ", summary);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private String summarizeText(String text) {
+        return "0";
+    }
+}
